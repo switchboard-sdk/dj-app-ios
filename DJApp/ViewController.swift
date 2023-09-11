0434b4ecec2694682d32b32e84f7527a70153a6b
Balazs Banto <balazsbanto@gmail.com>
2023-09-11 14:28:16 +0200

    Initial Commit

---
+++ DJApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DJApp
+//
+//  Created by Banto Balazs on 2023. 09. 11..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
